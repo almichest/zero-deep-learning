4f71bcccff4f4f7b21b6f67400e8233b9581ca35
Hiraku Ohno <hiraku.ohno@integrity-healthcare.co.jp>
2017-12-28 01:12:02 +0900

    to p27

--- p16.py
+++ p16.py
@@ -2,6 +2,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 x = np.arange(0, 6, 0.1)
-y = np.sin(x)
-plt.plot(x, y)
+y1 = np.sin(x)
+y2 = np.cos(x)
+y3 = np.cos(2 * x)
+plt.plot(x, y1, label='sin')
+plt.plot(x, y2, linestyle = '--', label='cos')
+plt.plot(x, y3, linestyle = '--', label='tan')
+plt.xlabel('x')
+plt.ylabel('y')
+plt.legend()
 plt.show()
